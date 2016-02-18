eb0018f5794a4a8a8b9f290dde5ebaacce10c23c
Steven Buller <steven@Stevens-MacBook-Pro.local>
2016-02-18 18:01:23 -0500

    students database is working

--- server.js
+++ server.js
@@ -24,15 +24,156 @@ app.use(session({
 }));
 
 
+var Student = sequelize.define('Student', {
+  firstname: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      len: {
+        args: [1, 20],
+        msg: "Please enter a first name less than 20 characters"
+      },
+      is: ["^[a-z]+$",'i']
+    }
+  },
+  lastname: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      len: {
+        args: [1, 20],
+        msg: "Please enter a last name less than 20 characters"
+      },
+      is: ["^[a-z]+$",'i']
+    }
+  },
+  email: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    unique: true
+  },
+  password: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      len: {
+        args: [1, 10],
+        msg: "Please enter a password between 1 and 10 characters"
+      }
+    }
+  }
+});
 
-
-
-
+var Instructor = sequelize.define('Instructor', {
+  firstname: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      len: {
+        args: [1, 20],
+        msg: "Please enter a first name less than 20 characters"
+      },
+      is: ["^[a-z]+$",'i']
+    }
+  },
+  lastname: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      len: {
+        args: [1, 20],
+        msg: "Please enter a last name less than 20 characters"
+      },
+      is: ["^[a-z]+$",'i']
+    }
+  },
+  email: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    unique: true
+  },
+  password: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      len: {
+        args: [1, 10],
+        msg: "Please enter a password between 1 and 10 characters"
+      }
+    }
+  }
+});
 
 app.get('/', function (req, res) {
-    res.render('home', {
+    res.render('registration', {
       msg: req.query.msg
     });
+});
+
+app.get('/login', function (req, res) {
+    res.render('login', {
+      msg: req.query.msg
+    });
+});
+
+app.get('/register', function (req, res) {
+    res.render('registration', {
+      msg: req.query.msg
+    });
+});
+
+app.get('/students', function (req, res) {
+    Student.findAll({
+    // include: [{
+    //   model: AlterEgo
+    //   }]
+    }).then(function(student) {
+      res.render('students' , {
+        student: student
+      })
+    });
+});
+
+app.get('/instructor', function (req, res) {
+    Instructor.findAll({
+    // include: [{
+    //   model: AlterEgo
+    //   }]
+    }).then(function(student) {
+      res.render('instructors' , {
+        instructor: instructor
+      })
+    });
+});
+
+app.post('/register', function(req, res) {
+  Student.create(req.body).then(function(user) {
+    req.session.authenticated = user;
+    res.redirect('/login');
+  }).catch(function(err) {
+    res.redirect('/register?msg=' + err.message);
+  });
+});
+
+app.post('/login', function(req, res) {
+  var email = req.body.email;
+  var password = req.body.password;
+
+  Student.findOne({
+    where: {
+      email: email,
+      password: password
+    }
+  }).then(function(user) {
+    if(user) {
+      req.session.authenticated = user;
+      res.redirect('/students');
+    } else {
+      res.redirect('/?msg=you are not logged in');
+    }
+  }).catch(function(err) {
+    throw err;
+  });
 });
 
 
